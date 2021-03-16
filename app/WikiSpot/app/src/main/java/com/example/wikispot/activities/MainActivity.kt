f53ed7653fc67a5708a6a74fb2ae7d34597b6877
Tucan444 <66880425+Tucan444@users.noreply.github.com>
2021-03-16 20:42:48 +0100

    Small changes

--- app/WikiSpot/app/src/main/java/com/example/wikispot/activities/MainActivity.kt
+++ app/WikiSpot/app/src/main/java/com/example/wikispot/activities/MainActivity.kt
@@ -2,23 +2,26 @@ package com.example.wikispot.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.wikispot.R
-import com.example.wikispot.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var binding: ActivityMainBinding
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirm")
+        builder.setMessage("Do you want to quit the application?")
+        builder.setPositiveButton("Yes") {_, _ -> finish()}
+        builder.setNegativeButton("No") {_, _ -> }
+        builder.show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
 
         val navController = findNavController(R.id.mainFragmentHost)
         val bottomNavView = findViewById<BottomNavigationView>(R.id.mainBottomNavigationView)
