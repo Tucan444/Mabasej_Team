a92ebde07d3d5f15c1e7cbe83ab3b88cfaf94193
Tucan444 <66880425+Tucan444@users.noreply.github.com>
2021-05-09 14:38:09 +0200

    _

--- websites/desc_short.js
+++ websites/desc_short.js
@@ -1,118 +1,86 @@
-// fetch("http://192.168.1.105:8000/devices_list")
-//             .then(function(response){
-//                 return response.json();          
-//             })
-//             .then(function(data){
-//                 appendData(data);
-//             })
-//             .catch(function(err){ 
-//                 console.log(err);
 
-//             });
-//             function appendData(data){
-//                 console.log(data);
-//                 console.log(data[1]["files"][0]);
-//                 var myJson = JSON.stringify(data[1]["description"]["description_s"]);
-//                 var myJson1 = JSON.stringify(data[1]["description"]["title"]);
-//                 var myJson2 = JSON.stringify(data[1]["description"]["description_l"]);
-//                 var uJson =  myJson.replace(/\"/g, "");
-//                 var str = (uJson);
-//                 var str_esc = escape(str);
-//                 console.log(str_esc + "<br>" + myJson1);
-//                 console.log(unescape(str_esc));
-//                 var mainContainer = document.getElementById("hou1");
-//                     var div = document.createElement("div");
-//                     div.setAttribute("id", "hou");
-//                     div.innerHTML = unescape(str_esc);
-
-//                     mainContainer.appendChild(div);
-
-//                 var mainContainer1 = document.getElementById("hey1");
-//                     var uJson1 = myJson1.replace(/\"/g, "");
-//                     var div1 = document.createElement("div");
-//                     div1.setAttribute("id", "hey");
-//                     div1.innerHTML = unescape(escape(uJson1));
-//                     var uJson1 = myJson1.replace(/\"/g, "");
-
-//                     mainContainer1.appendChild(div1);
-//                 }
 $(document).ready(function() {
-    let urls = ["http://192.168.1.105:8000/files/0/library.jpg", "http://192.168.1.105:8000/files/0/library.jpg", "http://192.168.1.105:8000/files/0/library.jpg"];
-    let descriptions = ["here", "there"];
-    let n = 1;
-    function append_element(urls, descriptions, n) {
+    let base_url = "http://192.168.1.156:8000/";
 
-        let template = `<div class="wsList hey">
-        <div ></div>
-        <img src="${urls[0]}" alt="#" style="height: 100px;" class="wsImg">
-        <div class="klol">
-                <div id="hey1"></div>
-                <div id="hou1"></div>
-        </div>
-        <div id="arrow${n}" class="arrow" onclick="onclick${n}()">&#9660</div>
-    </div>
-    <!-- Rozšírené malé zobrazenie -->
-    <div id="listExpand${n}" class="listHide">
-        <img src="${urls[1]}" alt="#" class="constantImg">
-        <div class="text">${descriptions[0]}</div>
-        <div>
-            <div class="senzory">Teplota</div>
-        <div id="heat" class="senzory"></div>
-        </div>
-        <div class="info"></div>
-    </div>
-    <!-- Veľké zobrazenie -->
-    <div class="big">
-        <img class="obrazok" src="${urls[2]}" alt="#">
-        <div class="text">${descriptions[1]}</div>
-    </div>`;
+    function append_element(image_urls, descriptions, title, id) {
+
+        let template = `
+            <div class="wsList hey">
+            <div ></div>
+            <img src="${image_urls[0]}" alt="#" style="height: 100px;" class="wsImg">
+            <div class="list_item_holder">
+                <div class="title${id}">${title}</div>
+                <div class="short_description${id}">${descriptions[0]}</div>
+            </div>
+            <div id="arrow${id}" class="arrow">&#9660</div>
+            </div>
+            <!-- Rozšírené malé zobrazenie -->
+            <div id="listExpand${id}" class="listHide">
+                <img src="${image_urls[1]}" alt="#" class="constantImg">
+                <div class="text">${descriptions[1]}</div>
+                <div class="sensors">
+                </div>
+                <div class="info"></div>
+            </div>
+            <!-- Veľké zobrazenie -->
+            <div class="big">
+                <img class="obrazok" src="${image_urls[1]}" alt="#">
+                <div class="text">${descriptions[1]}</div>
+            </div>`;
     
         $("body").append(template);
+
+        $.get(`http://192.168.1.156:8000/${id}/sensors`, function(data1) {
+            console.log(data1);
+            Object.keys(data1).forEach(function(key) {
+                let value = data1[key];
+                if (key.startsWith("[translate]-")) {
+                    key = key.slice(12, key.length);
+                }
+                $(".sensors").append(`<p>${key}: ${value}</p>`);
+            });
+        });
+
+        $(".arrow").click(function() {
+
+            let id = this.id;
+            id = id.slice(id.length-1, id.length);
+    
+            $(`#arrow${id}`).toggleClass("mystyle");
+            $(`#listExpand${id}`).toggleClass("expand");
+    
+        });
     
     }
-    append_element(urls, descriptions, n);
 
-    $.get("http://192.168.1.105:8000/devices_list", function(data) {
-    console.log(data); 
-    var myJson = JSON.stringify(data[1]["description"]["description_s"]);
-    var myJson1 = JSON.stringify(data[1]["description"]["title"]);
-    console.log(myJson);
-    console.log(myJson1);
-    uJson = myJson.replace(/\"/g, "");
-    var str = (uJson);
-    var str_esc = escape(str);
-    console.log(str_esc + "<br>" + myJson);
-    console.log(unescape(str_esc));
-    var main = document.getElementById("hou1");
-    var div = document.createElement("div");
-    div.setAttribute("id", "hou");
-    div.innerHTML = unescape(str_esc);
+    $.get(`${base_url}devices_list`, function(data) {
 
-        main.appendChild(div);
+        let connected_id = data[0]["connected_id"];
+        let urls = ["", ""];
+        let descriptions = ["", ""];
+        let title = "";
+        let query = [[]];
 
-    var sub = document.getElementById("hey1");
-    var uJson1 = myJson1.replace(/\"/g, "");
-    var div1 = document.createElement("div");
-    div1.setAttribute("id", "hey");
-    div1.innerHTML = unescape(escape(uJson1));
+        for (let i = 1; i < data.length; i++) {
+            let spot_data = data[i];
+            let spot_id = spot_data["ID"]
+            urls = [`${base_url}files/${spot_id}/${spot_data["description"]["photo_s"]}`,
+                    `${base_url}files/${spot_id}/${spot_data["description"]["photo_b"]}`];
+            descriptions = [spot_data["description"]["description_s"],
+                            spot_data["description"]["description_l"]];
+            title = spot_data["description"]["title"];
 
-        sub.appendChild(div1);
+            if (spot_id == connected_id) {
+                query[0] = [urls, descriptions, title, parseInt(spot_id)];
+            } else {
+                query.push([urls, descriptions, title, parseInt(spot_id)]);
+            }
+        }
 
-
-
-    ID = 0;
-    $.get(`http://192.168.1.105:8000/${ID}/sensors`, function(data1) {
-        console.log(data1);
-        var sens = JSON.stringify(data1["teplota"]);
-        console.log(sens);
-        var uSens = sens.replace(/\"/g, "");
-        var heat = document.getElementById("heat");
-        var div2 = document.createElement("div");
-        div2.setAttribute("id", "hot");
-        div2.innerHTML = unescape(escape(uSens));
-
-        heat.appendChild(div2);
-})
+        for (let i = 0; i < query.length; i++) {
+            append_element(query[i][0], query[i][1], query[i][2], query[i][3]);
+        }
+ 
     })
     
 })
