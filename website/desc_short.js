526315d65c71e4611f9cd19ee32da2ecf216888e
Tucan444 <66880425+Tucan444@users.noreply.github.com>
2021-05-10 19:15:52 +0200

    _

--- website/desc_short.js
+++ website/desc_short.js
@@ -1,14 +1,22 @@
 
 $(document).ready(function() {
-    let base_url = "http://localhost:8000/";
+    let base_url = "http://lohost:8000/";
+    let ids_list = [];
+
+    $.get("ip.txt", function(data) {
+        console.log(data);
+        base_url = `http://${data}:8000/`;
+        init_wesite();
+    });
 
     function append_element(image_urls, descriptions, title, id) {
+        ids_list.push(id);
 
         let template = `
             <div class="wsList hey">
             <div></div>
             <img src="${image_urls[0]}" alt="#" class="obrazok_small">
-            <div class="list_item_holder">
+            <div id="list_item_holder${id}" class="list_item_holder">
                 <div class="title${id}">${title}</div>
                 <div class="short_description">${descriptions[0]}</div>
             </div>
@@ -23,7 +31,7 @@ $(document).ready(function() {
                 <div class="info_small"></div>
             </div>
             <!-- Veľké zobrazenie -->
-            <div class="big">
+            <div id="big${id}" class="big">
                 <div class="imgBig"><img class="obrazok_big" src="${image_urls[1]}" alt="#"></div>
                 <div class="text_big">${descriptions[1]}</div>
                 <div class="sensors_big">
@@ -53,37 +61,54 @@ $(document).ready(function() {
             $(`#listExpand${id}`).toggleClass("expand");
             id.a();
         });
+
+        $(".list_item_holder").click(function() {
+            let id = this.id;
+            id = id.slice(id.length-1, id.length);
+            
+            clear_big_display();
+            $(`#big${id}`).show();
+        });
     
     }
 
-    $.get(`${base_url}devices_list`, function(data) {
+    function clear_big_display(){
+        ids_list.forEach(function(id) {
+            $(`#big${id}`).hide();
+        });
+    }
 
-        let connected_id = data[0]["connected_id"];
-        let urls = ["", ""];
-        let descriptions = ["", ""];
-        let title = "";
-        let query = [[]];
+    function init_wesite() {
+        $.get(`${base_url}devices_list`, function(data) {
 
-        for (let i = 1; i < data.length; i++) {
-            let spot_data = data[i];
-            let spot_id = spot_data["ID"]
-            urls = [`${base_url}files/${spot_id}/${spot_data["description"]["photo_s"]}`,
-                    `${base_url}files/${spot_id}/${spot_data["description"]["photo_b"]}`];
-            descriptions = [spot_data["description"]["description_s"],
-                            spot_data["description"]["description_l"]];
-            title = spot_data["description"]["title"];
-
-            if (spot_id == connected_id) {
-                query[0] = [urls, descriptions, title, parseInt(spot_id)];
-            } else {
-                query.push([urls, descriptions, title, parseInt(spot_id)]);
-            }
-        }
-
-        for (let i = 0; i < query.length; i++) {
-            append_element(query[i][0], query[i][1], query[i][2], query[i][3]);
-        }
- 
-    })
+            let connected_id = data[0]["connected_id"];
+            let urls = ["", ""];
+            let descriptions = ["", ""];
+            let title = "";
+            let query = [[]];
     
+            for (let i = 1; i < data.length; i++) {
+                let spot_data = data[i];
+                let spot_id = spot_data["ID"]
+                urls = [`${base_url}files/${spot_id}/${spot_data["description"]["photo_s"]}`,
+                        `${base_url}files/${spot_id}/${spot_data["description"]["photo_b"]}`];
+                descriptions = [spot_data["description"]["description_s"],
+                                spot_data["description"]["description_l"]];
+                title = spot_data["description"]["title"];
+    
+                if (spot_id == connected_id) {
+                    query[0] = [urls, descriptions, title, parseInt(spot_id)];
+                } else {
+                    query.push([urls, descriptions, title, parseInt(spot_id)]);
+                }
+            }
+    
+            for (let i = 0; i < query.length; i++) {
+                append_element(query[i][0], query[i][1], query[i][2], query[i][3]);
+            }
+     
+        })
+    }
+    
+    init_wesite();
 })
